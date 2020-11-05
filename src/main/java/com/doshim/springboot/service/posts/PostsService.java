92739312e27a8fc4998d06bee86db3d61728c8eb
Daniel DR Shim <doshim@coupang.com>
2020-11-05 23:56:52 +0900

    Ch 4. Mustache but hellocontroller test fail.

--- src/main/java/com/doshim/springboot/service/posts/PostsService.java
+++ src/main/java/com/doshim/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.doshim.springboot.service.posts;
 
 import com.doshim.springboot.domain.posts.Posts;
 import com.doshim.springboot.domain.posts.PostsRepository;
+import com.doshim.springboot.web.dto.PostsListResponseDto;
 import com.doshim.springboot.web.dto.PostsResponseDto;
 import com.doshim.springboot.web.dto.PostsSaveRequestDto;
 import com.doshim.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -24,6 +28,7 @@ public class PostsService {
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(() -> new
                         IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        //java8
 
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
@@ -34,7 +39,25 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new
                         IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        //java8
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+        //java8
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new
+                        IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
